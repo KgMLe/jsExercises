98516a03b407c95c2683bc2bf5093fe508f24e7e
KgMLe <kgodisholebopa@gmail.com>
2023-05-24 15:25:55 +0200

    Question1

---
+++ script/code.js
@@ -0,0 +1,29 @@
+// 1. Variable
+// Variables are place holders. They are like a plastci bag but you have to put items inside that the plastic bag will hold.
+// 2. Value
+// This refers to the data stored within a variable.
+// 3. Data type
+// Data types describe the different types or kinds of data that we work with and store in variables. 
+// 4. Initialization
+// When we store a value in a variable
+// 5. Tell me the difference between let, var, and const.
+// const creates variables which can not be reassignedd and are a definite variable, let has values which can be reassigned and var 
+// 6. What is a variable scope?
+// scope of a variable is its lifetime in the program
+// 7. When can you decide when to use a for or while, forEach and do while loop?
+// It depends on the task at hand. All the functions differ 
+// 8. Define hoisting and variable scoping
+// Hoisting is the process of moving function declarations to the top of the file. 
+// scope of a variable is its lifetime in the program
+
+function fullName() {
+    let firstName = "Kgodisho"
+    let lastName = "Lebopa"
+    let fullName = `Hi my fullname is ${firstName}${lastName}` 
+  
+    console.log(fullName);
+    return fullName;
+  }
+  
+  fullName();
+  
