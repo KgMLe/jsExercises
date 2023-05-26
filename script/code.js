adb0d2200d39dfac4274aa2d05a4efa0a282c5b3
KgMLe <kgodisholebopa@gmail.com>
2023-05-26 12:22:38 +0200

    Q3Final

--- script/code.js
+++ script/code.js
@@ -5,53 +5,72 @@
 // 2. Value
 // This refers to the data stored within a variable.
 // 3. Data type
-// Data types describe the different types or kinds of data that we work with and store in variables. 
+// Data types describe the different types or kinds of data that we work with and store in variables.
 // 4. Initialization
 // When we store a value in a variable
 // 5. Tell me the difference between let, var, and const.
-// const creates variables which can not be reassignedd and are a definite variable, let has values which can be reassigned and var 
+// const creates variables which can not be reassignedd and are a definite variable, let has values which can be reassigned and var
 // 6. What is a variable scope?
 // scope of a variable is its lifetime in the program
 // 7. When can you decide when to use a for or while, forEach and do while loop?
-// It depends on the task at hand. All the functions differ 
+// It depends on the task at hand. All the functions differ
 // 8. Define hoisting and variable scoping
-// Hoisting is the process of moving function declarations to the top of the file. 
+// Hoisting is the process of moving function declarations to the top of the file.
 // scope of a variable is its lifetime in the program
 
-function fullName() {
-    let firstName = "Kgodisho"
-    let lastName = "Lebopa"
-    let fullName = `Hi my fullname is ${firstName}${lastName}` 
-  
-    console.log(fullName);
-    return fullName;
-  }
-  
-  fullName();
- //______________________________________________________________________________ 
+// function fullName() {
+//     let firstName = "Kgodisho"
+//     let lastName = "Lebopa"
+//     let fullName = `Hi my fullname is ${firstName}${lastName}`
+
+//     console.log(fullName)
+//   }
+
+//   fullName()
+//______________________________________________________________________________
 
 //   QUESTION 2
 
+// let myname = 'Kgodisho'
+// let surname = 'Lebopa'
+// let age = 24
+// subjects = ['CSS','HTML','BOOTSTRAP','javascript']
+// let address = {
+//      strname : 'Lower Church street' ,
+//      strno : 1 ,
+//      suburb : 'Wynberg ',
+//      city : 'Cape Town' ,
+//     country : 'South Africa'
 
-let myname = 'Kgodisho'
-let surname = 'Lebopa'
-let age = 24
-subjects = ['CSS','HTML','BOOTSTRAP','javascript']
-let address = {
-     strname : 'Lower Church street' ,
-     strno : 1 ,
-     suburb : 'Wynberg ',
-     city : 'Cape Town' ,
-    country : 'South Africa'
+// }
+
+// console.log(myname);
+// console.log(surname);
+// console.log(age);
+// console.log(subjects);
+// console.log(address);
+
+//________________________________________________________________________________
+// QUESTION 3
+
+function calcResult() {
+  let num1 = parseFloat (document.querySelector("#num1").value);
+  let op = document.querySelector("#op").value;
+  let num2 = parseFloat (document.querySelector("#num2").value);
+  let lblResult = document.querySelector("#lblResult");
+
+  if (op == "+") {
+    lblResult.textContent = num1 + num2;
+  } else if (op == "-") {
+    lblResult.textContent = num1 - num2;
+  } else if (op == "*") {
+    lblResult.textContent = num1 * num2;
+  } else if (op == "/") {
+    lblResult.textContent = num1 / num2;
+  } else {
+     lblResult.textContent = "error";
+ }
+ console.log(lblResult);
 
 }
 
-console.log(myname);
-console.log(surname);
-console.log(age);
-console.log(subjects);
-console.log(address);
-
-//________________________________________________________________________________
-
-    
